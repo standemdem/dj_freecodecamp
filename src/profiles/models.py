0e249e8a3d58b004994a28d03dd9e0833a54a68f
standemdem <stan.demontmarin@gmail.com>
2021-04-26 11:37:44 +0200

    fixed typo

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(_(default = "no bio..."))
+    bio = models.TextField(default = "no bio...")
     avatar = models.ImageField(upload_to="avatars", default = "no_picture.png")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"profile of {self.user}"
+        return f"profile of {self.user.username}"
     
